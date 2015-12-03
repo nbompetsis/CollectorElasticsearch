8447fc3deca7dcbff4892362927e3d7d0db917cb
Nikolas Bompetsis <nikolas.mpompetsis@gmail.com>
2015-12-03 03:13:20 +0200

    Integration with Elastic Search

--- collector.py
+++ collector.py
@@ -1,31 +1,46 @@
 import beanstalkc
 import json
 from varlib import MYHOST
-
+from elasticsearch import Elasticsearch
 
 def main():
     beanstalk = beanstalkc.Connection(host=MYHOST, port=11301)
+    es = Elasticsearch()
+
+    # ignore 400 cause by IndexAlreadyExistsException when creating an index
+    es.indices.create(index='grmoto', ignore=400)
+
+    # ignore 404 and 400
+    es.indices.delete(index='grmoto', ignore=[400, 404])
 
 
-    while True:
-
-        # To receive a job:
-        job = beanstalk.reserve()
+    try:
+        while True:
+            # To receive a job:
+            job = beanstalk.reserve()
         
-#       if job.body == 'quit':
-#           print 'The agent shutting down'
-#           break
+#           if job.body == 'quit':
+#               print 'The agent shutting down'
+#               break
 
 
-        # Work with the job:
-        obj = json.loads(job.body)
-        
-        print json.dumps(obj, sort_keys=True, indent=4, separators=(',',': '))
+            # Work with the job:
+            obj = json.loads(job.body)
+            
+            #print json.dumps(obj, sort_keys=True, indent=4, separators=(',',': '))
+
+            #use elastic search 
+            res = es.index(index='grmoto', doc_type='native_objects', body = obj)
+            print(res['created'])
+            
+            #Release the job
+            job.delete()
+    except:
+        again()
 
 
-        job.delete()
-
-
+def again():
+    main()
 
 
 if __name__ == "__main__":
